1db6d492fd06cb8b03222bdbc951a5372be9baa4
runbuilder <154466890+Runbuilder@users.noreply.github.com>
2024-10-11 13:09:49 +0900

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -18,7 +18,7 @@ const CardContainer = styled.div`
   box-shadow: 0 4px 6px rgba(0,0,0,0.1);
 
   &:hover {
-    transform: translateY(-5px);
+    transform: translateY(-3px);translateX(-3px)
     box-shadow: 0 8px 15px rgba(0,0,0,0.2)
   }
 
