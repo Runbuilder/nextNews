75715ba14b94e4a2822d64dd81a7a96fb97a17e5
runbuilder <154466890+Runbuilder@users.noreply.github.com>
2024-10-11 11:57:05 +0900

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -13,7 +13,7 @@ const CardContainer = styled.div`
   text-align: center;
   cursor: pointer;
   // background-color: ${props => props.$backgroundColor || '#FFFFFF'}; // 기본값으로 흰색 설정
-  background: linear-gradient(115deg, #4e54c8, #8f94fb);
+  background: linear-gradient(115deg,#2980B9, #6DD5FA,#FFFFFF);
   position: relative;
   transition: transform 0.3s ease, box-shadow 0.3s ease;
 
